b72675f228f2dfd10f2309e0ccf1ab7e87b2d30f
Rindra <hei.rindra.2@gmail.com>
2026-07-03 09:21:39 +0300

    create: course service

---
+++ src/main/java/mg/yoan/course/service/CourseService.java
@@ -0,0 +1,17 @@
+package mg.yoan.course.service;
+
+import lombok.AllArgsConstructor;
+import mg.yoan.course.model.Course;
+import mg.yoan.course.repository.CourseRepository;
+import org.springframework.stereotype.Service;
+
+@Service
+@AllArgsConstructor
+public class CourseService {
+
+    private final CourseRepository courseRepository;
+
+    public Course create(Course course) {
+        return courseRepository.save(course);
+    }
+}
