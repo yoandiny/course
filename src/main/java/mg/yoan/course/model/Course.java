ddbbfe4797b4276f53112d2569e36a4fdc6c1b78
Rindra <hei.rindra.2@gmail.com>
2026-07-03 09:21:01 +0300

    create: course model

---
+++ src/main/java/mg/yoan/course/model/Course.java
@@ -0,0 +1,28 @@
+package mg.yoan.course.model;
+
+import jakarta.persistence.*;
+import java.time.Instant;
+import java.util.UUID;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "course")
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class Course {
+
+    @Id
+    @GeneratedValue
+    private UUID id;
+
+    private String name;
+
+    private Instant startDate;
+
+    private Instant endDate;
+}
