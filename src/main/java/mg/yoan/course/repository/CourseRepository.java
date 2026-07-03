fae249be911278e0f083426db7aac59dbc772442
Rel4w <hei.jonathan.4@gmail.com>
2026-07-03 09:43:54 +0300

    fix: error

--- src/main/java/mg/yoan/course/repository/CourseRepository.java
+++ src/main/java/mg/yoan/course/repository/CourseRepository.java
@@ -6,7 +6,6 @@ import mg.yoan.course.repository.model.Course;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-
 @PojaGenerated
 @Repository
 public interface CourseRepository extends JpaRepository<Course, UUID> {}
--- src/main/java/mg/yoan/course/repository/UserRepository.java
+++ src/main/java/mg/yoan/course/repository/UserRepository.java
@@ -1,12 +1,10 @@
 package mg.yoan.course.repository;
 
 import java.util.UUID;
-
 import mg.yoan.course.PojaGenerated;
 import mg.yoan.course.repository.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
-
 
 @PojaGenerated
 @Repository
