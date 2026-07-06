549b2f96febde7dcd1a074ccfc8f6f0c381b8a4a
Rindra <hei.rindra.2@gmail.com>
2026-07-06 11:36:07 +0300

    style: format indentation with ./format.sh

--- src/main/java/mg/yoan/course/endpoint/rest/controller/CourseController.java
+++ src/main/java/mg/yoan/course/endpoint/rest/controller/CourseController.java
@@ -1,5 +1,6 @@
 package mg.yoan.course.endpoint.rest.controller;
 
+import java.util.List;
 import lombok.AllArgsConstructor;
 import mg.yoan.course.repository.model.Course;
 import mg.yoan.course.service.CourseService;
@@ -7,23 +8,21 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.List;
-
 @RestController
 @RequestMapping("/course")
 @AllArgsConstructor
 public class CourseController {
 
-    private final CourseService courseService;
+  private final CourseService courseService;
 
-    @PostMapping
-    public ResponseEntity<Course> create(@RequestBody Course course) {
-        var created = courseService.create(course);
-        return ResponseEntity.status(HttpStatus.CREATED).body(created);
-    }
+  @PostMapping
+  public ResponseEntity<Course> create(@RequestBody Course course) {
+    var created = courseService.create(course);
+    return ResponseEntity.status(HttpStatus.CREATED).body(created);
+  }
 
-    @GetMapping
-    public ResponseEntity<List<Course>> getAllCourses() {
-        return ResponseEntity.ok(courseService.getAll());
-    }
+  @GetMapping
+  public ResponseEntity<List<Course>> getAllCourses() {
+    return ResponseEntity.ok(courseService.getAll());
+  }
 }
